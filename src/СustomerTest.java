7ed0241178133303dc113f9d5ff74d1bec35e292
Jekaprio <59193602+Jekaprio@users.noreply.github.com>
2020-11-13 21:12:53 +0300

    Add files via upload

---
+++ src/СustomerTest.java
@@ -0,0 +1,39 @@
+public class Customer {
+	private final int ID;
+	private boolean isNew = true;
+	private double total = 1000;
+
+
+    public Customer() {
+		this.ID = 1;
+    		this.isNew = true;
+    		this.total = 1000;
+	}
+
+  public int getID() {
+		return ID;
+	}
+
+
+	public boolean isNew() {
+		return isNew;
+	}
+
+	public void setNew(boolean isNew) {
+		this.isNew = isNew;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public void displayCustomerInfo() {
+		System.out.println("ID is " + this.ID);
+		System.out.println("Is new: " + this.isNew);
+		System.out.println("Total is " + this.total);
+	}
+}
