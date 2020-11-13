2fd9d7d6bf635072a2e9d6d6654bfee9dbf1f496
Jekaprio <59193602+Jekaprio@users.noreply.github.com>
2020-11-14 02:13:16 +0300

    Update СustomerTest.java

--- src/СustomerTest.java
+++ src/СustomerTest.java
@@ -1,39 +1,9 @@
-public class Customer {
-	private final int ID;
-	private boolean isNew = true;
-	private double total = 1000;
-
-
-    public Customer() {
-		this.ID = 1;
-    		this.isNew = true;
-    		this.total = 1000;
-	}
-
-  public int getID() {
-		return ID;
-	}
-
-
-	public boolean isNew() {
-		return isNew;
-	}
-
-	public void setNew(boolean isNew) {
-		this.isNew = isNew;
-	}
-
-	public double getTotal() {
-		return total;
-	}
-
-	public void setTotal(double total) {
-		this.total = total;
-	}
-
-	public void displayCustomerInfo() {
-		System.out.println("ID is " + this.ID);
-		System.out.println("Is new: " + this.isNew);
-		System.out.println("Total is " + this.total);
+public class СustomerTest {
+ public static void main(String[] args) {
+		Сustomer c = new Сustomer();
+    c.setID(77);
+    c.setNew(false);
+    c.setTotal(666.66);
+    c.displayCustomerInfo();
 	}
 }
